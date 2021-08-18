888563d6cf019102179e3c86998c030ae8f3bbea
Matt B <matthew.bowler123@gmail.com>
2021-08-18 19:07:33 -0400

    fixing the way types are declared for plugins

--- web/components/content-page.component.vue
+++ web/components/content-page.component.vue
@@ -5,23 +5,24 @@
         <!-- <h1 class='fv-margin-bottom fv-text-center title-text'>About Me</h1> -->
         <div class="image-container">
             <div class="image-text">
-                this is some text that is inspiring
+                Want to expand your carrer? Schedule A Consult.
 
-                <NuxtLink class="fv-button fv-secondary image-button" to="/"> Do Something </NuxtLink>
+                <a class="fv-button fv-secondary image-button" href="/coaching"> Do Something </a>
             </div>
         </div>
-        <p><em>Professional Carrer, Life, and Relationship Coach in Groveland, MA</em>  </p>
-        <p><strong>“Knowing others is wisdom, knowing yourself is enlightenment.” Lao Tzu</strong>  </p>
-        <p><strong>Are you a high performer and dedicated to bringing out the best in yourself, your team, employees or clients? Do you want to develop yourself to be the best that you can be? Do you want to become a more powerful Influencer? Are you interested in learning to use Emotional Intelligence to modify habits and self-limiting beliefs so that your strengths will flourish?</strong>  </p>
-        <p><strong>Do you want to use mindfulness, improve communication in your relationships, resolve and reduce conflict and bring your relationship to a higher level?</strong>  </p>
-        <p><strong>Are you a businesses owner who wants to take your business to the next levels of success?</strong>  </p>
-        <p>I&#39;m Richard Bowler, a Professional Growth, Relationship and Executive Coach with over 30 years of experience mentoring and guiding people. My goal is to develop a trusting coaching relationship, to provide clarity, advance your thinking, offer fresh perspectives, bring solutions that help you accomplish your unique goals! I work globally with individuals and business teams.</p>
+        <div class="color-text-container">
+            <div class="information-color-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium mattis ligula eu feugiat. Duis quis sem ac justo fermentum congue in sed enim. Phasellus id felis eget quam accumsan porta a quis leo. Nam sed lorem et mi vehicula rutrum ac eget quam. Suspendisse varius consequat felis ut porttitor. Mauris eget risus hendrerit nulla faucibus efficitur nec ac nibh. Sed in ullamcorper dui. Curabitur cursus mauris a ex la
+            </div>
+            <div class="">
+                <img width="350" src="/final-02.png" />
+            </div>
+        </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import '@nuxt/types'
 import marked from "marked";
 import { Vue, Component } from "vue-property-decorator";
 import { apiCall } from '../library/api.helper';
@@ -39,6 +40,8 @@ export default class ContentPage extends Vue {
                 this.$data.pageText = response.data.pageTexts[0].markdown
             }
         })
+
+        const fo = this.$mq;
     }
     pageMarkdown() {
         return marked(this.$data.pageText);
@@ -50,7 +53,6 @@ export default class ContentPage extends Vue {
 @import url('https://fonts.googleapis.com/css?family=Noto+Serif');
 .content {
     margin: 2.5em auto;
-    max-width: 70%;
 }
 .title-text {
     font-family: 'Noto Serif';
@@ -69,6 +71,15 @@ export default class ContentPage extends Vue {
     justify-content: flex-end;
 }
 
+.color-text-container {
+    width: 100%;
+    height: 25vh;
+    align-items: center;
+    background-color: #6A957F;
+    display: flex;
+    justify-content: flex-start;
+}
+
 .image-text {
     width: 30%;
     color: white;
@@ -78,7 +89,24 @@ export default class ContentPage extends Vue {
     word-wrap: break-word;
 }
 
+.information-color-text {
+    width: 75%;
+    color: white;
+    font-weight: bolder;
+    font-size: 24px;
+    padding: 70px;
+    word-wrap: break-word;
+}
+
 .image-button {
     font-size: 14px;
+    background: #B90101;
+    border: none !important;
+    box-shadow: none;
+    text-shadow: none;
+}
+
+.image-button:hover {
+    background: #B90101 !important;
 }
 </style>
